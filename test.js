c45f1fd8e25dc5a55af6d4fef6132dd0a3b50b55
rayan <111146442+themohammedrayan@users.noreply.github.com>
2025-11-01 16:16:42 +0000

    set

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log('Rayan')
+// your JavaScript file
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
